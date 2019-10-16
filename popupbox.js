2988e09f06fb689dea2b8bfc54a7cc9fc2787578
Lakshmi-coder <56440820+Lakshmi-coder@users.noreply.github.com>
2019-10-16 17:09:03 +0530

    Add files via upload

---
+++ popupbox.js
@@ -0,0 +1,12 @@
+function MyAlert()
+{
+    alert("welcome to Extensionjs");
+}
+function MyConfirm()
+{
+    confirm("click ok or cancel");
+}
+function MyPrompt()
+{
+    prompt("Enter any value:","100");
+}
